d4adc772c522f3eff6bba05fbe100c5b489d050e
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2026-02-09 19:40:00 +0700

    feat(tools): MenuItem to refresh UPM files (manifest.json, packages-lock.json)

---
+++ Packages/com.soratehk.sorafoundation/Editor/Extensions/PackageManagerX.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+namespace SoraTehk.Extensions {
+    public static class PackageManagerX {
+        public static async Awaitable WaitForCompletion(this Request req, CancellationToken ct = default) {
+            while (!req.IsCompleted && !ct.IsCancellationRequested) {
+                await Awaitable.NextFrameAsync(ct);
+            }
+            ct.ThrowIfCancellationRequested();
+        }
+    }
+}
---
+++ Packages/com.soratehk.sorafoundation/Editor/MenuItems/UPMMenuItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using SoraTehk.Extensions;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace SoraTehk.Tools {
+    public static partial class SoraTehkMenuItem {
+        [MenuItem("Tools/SoraTehk/RefreshUPM")]
+        // ReSharper disable once Unity.IncorrectMethodSignature
+        public static async Awaitable RefreshUnityPackageManager() {
+            try {
+                string dummyPackageId = "com.unity.collab-proxy";
+
+                EditorUtility.DisplayProgressBar("Refreshing UPM", "Checking dummy package installation", 0.1f);
+                var listReq = Client.List(true);
+                await listReq.WaitForCompletion();
+                EditorUtility.ClearProgressBar();
+
+                bool dummyPackageInstalled = listReq.Result.Any(p => p.name == dummyPackageId);
+
+                if (dummyPackageInstalled) {
+                    Client.Remove(dummyPackageId);
+                }
+                else {
+                    Client.Add(dummyPackageId);
+                }
+            }
+            finally {
+                EditorUtility.ClearProgressBar();
+            }
+        }
+    }
+}

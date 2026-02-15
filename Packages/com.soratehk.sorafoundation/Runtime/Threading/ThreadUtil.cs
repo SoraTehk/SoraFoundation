c21ed7f915815ec7c7f28259e4f222cc9410d481
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2026-02-15 22:57:31 +0700

    feat(thread): Static method to check if current code running on Unity's main-thread or not

---
+++ Packages/com.soratehk.sorafoundation/Runtime/Threading/ThreadUtil.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using SoraTehk.Attributes;
+
+namespace SoraFoundation.Threading {
+    [PreloadStatic]
+    public static class ThreadUtil {
+        static ThreadUtil() {
+            MAIN_THREAD_ID = Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public static int MAIN_THREAD_ID { get; }
+
+        public static bool G_IsMainThread() => Thread.CurrentThread.ManagedThreadId == MAIN_THREAD_ID;
+    }
+}

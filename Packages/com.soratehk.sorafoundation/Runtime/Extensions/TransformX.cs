14da62a478b40e7d60b4daa78fe7941c03071b97
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2026-03-04 12:55:09 +0700

    feat(extensions): Add various method to find reference in/on Scene, Transform, Project

---
+++ Packages/com.soratehk.sorafoundation/Runtime/Extensions/TransformX.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace SoraTehk.Extensions {
+    public static partial class TransformX {
+        public static bool TryFindFirstComponent<T>(this Transform transform, out T? result, string? gameObjName = null)
+            where T : Component {
+            //
+            result = null;
+            bool checkName = !string.IsNullOrEmpty(gameObjName);
+            
+            if (checkName) {
+                T[] cpns = transform.GetComponentsInChildren<T>(true);
+                foreach (var c in cpns) {
+                    if (c.gameObject.name != gameObjName) continue;
+                    
+                    result = c;
+                    return true;
+                }
+            }
+            else {
+                result = transform.GetComponentInChildren<T>(true);
+                if (result != null) {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}

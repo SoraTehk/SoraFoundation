967a8025c7f3a5b12053b3423793c5924a604617
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2026-03-04 09:42:50 +0700

    bck: UniqueStack

---
+++ Packages/com.soratehk.sorafoundation/Editor/Tests/UniqueStackTests.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SoraTehk.Collections;
+
+namespace SoraTehk.Tests {
+    public class UniqueStackTests {
+        [Test]
+        public void AddAndRemove_ShouldMaintainStackIntegrity() {
+            var stack = new UniqueStack<int>();
+            stack.Add(1);
+            stack.Add(2);
+            stack.Push(3);
+            stack.Remove(2);
+            
+            Assert.AreEqual(2, stack.Count);
+            CollectionAssert.AreEqual(new[] { 3, 1 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void Add_ShouldAddItem_WhenNotExists() {
+            var stack = new UniqueStack<int>();
+            stack.Add(1);
+            
+            Assert.AreEqual(1, stack.Count);
+            Assert.IsTrue(stack.Contains(1));
+        }
+        
+        [Test]
+        public void Add_ShouldNotAddDuplicate() {
+            var stack = new UniqueStack<int>();
+            stack.Add(1);
+            stack.Add(1);
+            
+            Assert.AreEqual(1, stack.Count);
+        }
+        
+        [Test]
+        public void Clear_ShouldRemoveAllItems() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Clear();
+            
+            Assert.AreEqual(0, stack.Count);
+            Assert.IsFalse(stack.Contains(1));
+        }
+        
+        [Test]
+        public void Contains_ShouldReturnFalseForNonExistingItem() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            Assert.IsFalse(stack.Contains(5));
+        }
+        
+        [Test]
+        public void Contains_ShouldReturnTrueForExistingItem() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            Assert.IsTrue(stack.Contains(2));
+        }
+        
+        [Test]
+        public void Constructor_ShouldPopulateStackAndPreserveOrder() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3, 1, 4 });
+            Assert.AreEqual(4, stack.Count);
+            CollectionAssert.AreEqual(new[] { 4, 3, 2, 1 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void Enumerator_ShouldIterateFromTopToBottom() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            var items = stack.ToArray();
+            
+            CollectionAssert.AreEqual(new[] { 3, 2, 1 }, items);
+        }
+        
+        [Test]
+        public void Enumerator_ShouldReflectChangesAfterInsertAndRemove() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Insert(1, 99);
+            stack.RemoveAt(0);
+            
+            var items = stack.ToArray();
+            CollectionAssert.AreEqual(new[] { 99, 2, 1 }, items);
+        }
+        
+        [Test]
+        public void IndexOf_ShouldReturnCorrectStackIndex() {
+            var stack = new UniqueStack<int>(new[] { 10, 20, 30 });
+            Assert.AreEqual(0, stack.IndexOf(30));
+            Assert.AreEqual(2, stack.IndexOf(10));
+            Assert.AreEqual(-1, stack.IndexOf(99));
+        }
+        
+        [Test]
+        public void IndexOf_ShouldReturnCorrectStackIndexAfterInsertions() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Insert(0, 4);
+            stack.Insert(2, 5);
+            
+            Assert.AreEqual(0, stack.IndexOf(4));
+            Assert.AreEqual(1, stack.IndexOf(3));
+            Assert.AreEqual(2, stack.IndexOf(5));
+            Assert.AreEqual(3, stack.IndexOf(2));
+            Assert.AreEqual(4, stack.IndexOf(1));
+        }
+        
+        [Test]
+        public void Insert_ShouldHandleExistingItemAtSameIndex() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Insert(1, 2);
+            
+            Assert.AreEqual(3, stack.Count);
+            CollectionAssert.AreEqual(new[] { 3, 2, 1 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void Insert_ShouldInsertAtBottomWhenIndexCount() {
+            var stack = new UniqueStack<int>(new[] { 1, 2 });
+            stack.Insert(2, 99);
+            
+            Assert.AreEqual(3, stack.Count);
+            Assert.AreEqual(2, stack.IndexOf(99));
+            CollectionAssert.AreEqual(new[] { 2, 1, 99 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void Insert_ShouldInsertAtCorrectStackIndex() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Insert(1, 99);
+            
+            Assert.AreEqual(4, stack.Count);
+            Assert.AreEqual(0, stack.IndexOf(3));
+            Assert.AreEqual(1, stack.IndexOf(99));
+            Assert.AreEqual(2, stack.IndexOf(2));
+        }
+        
+        [Test]
+        public void Insert_ShouldInsertAtTopWhenIndexZero() {
+            var stack = new UniqueStack<int>(new[] { 1, 2 });
+            stack.Insert(0, 99);
+            
+            Assert.AreEqual(3, stack.Count);
+            Assert.AreEqual(0, stack.IndexOf(99));
+            CollectionAssert.AreEqual(new[] { 99, 2, 1 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void Insert_ShouldMoveExistingItemToNewIndex() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Insert(0, 2);
+            
+            Assert.AreEqual(3, stack.Count);
+            Assert.AreEqual(0, stack.IndexOf(2));
+            Assert.AreEqual(1, stack.IndexOf(3));
+            Assert.AreEqual(2, stack.IndexOf(1));
+        }
+        
+        [Test]
+        public void Pop_ShouldReturnTopItemAndRemoveIt() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            int top = stack.Pop();
+            
+            Assert.AreEqual(3, top);
+            Assert.AreEqual(2, stack.Count);
+            Assert.IsFalse(stack.Contains(3));
+        }
+        
+        [Test]
+        public void Pop_ShouldWorkCorrectlyAfterInsertions() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Insert(1, 99);
+            int top = stack.Pop();
+            
+            Assert.AreEqual(3, top);
+            Assert.AreEqual(3, stack.Count);
+            CollectionAssert.AreEqual(new[] { 99, 2, 1 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void Pop_ShouldThrowException_WhenEmpty() {
+            var stack = new UniqueStack<int>();
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+        
+        [Test]
+        public void Push_ShouldAddItemOnTop() {
+            var stack = new UniqueStack<int>(new[] { 1, 2 });
+            stack.Push(3);
+            
+            Assert.AreEqual(3, stack.Count);
+            Assert.AreEqual(0, stack.IndexOf(3));
+        }
+        
+        [Test]
+        public void Push_ShouldHandleMultipleDuplicatesCorrectly() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Push(2);
+            stack.Push(2);
+            
+            Assert.AreEqual(3, stack.Count);
+            Assert.AreEqual(0, stack.IndexOf(2));
+            CollectionAssert.AreEqual(new[] { 2, 3, 1 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void Push_ShouldMoveExistingItemToTop() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.Push(2);
+            
+            Assert.AreEqual(3, stack.Count);
+            Assert.AreEqual(0, stack.IndexOf(2));
+            Assert.AreEqual(1, stack.IndexOf(3));
+            Assert.AreEqual(2, stack.IndexOf(1));
+        }
+        
+        [Test]
+        public void Remove_ShouldRemoveExistingItem() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            bool removed = stack.Remove(2);
+            
+            Assert.IsTrue(removed);
+            Assert.AreEqual(2, stack.Count);
+            Assert.IsFalse(stack.Contains(2));
+        }
+        
+        [Test]
+        public void Remove_ShouldReturnFalseForNonExistingItem() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            bool removed = stack.Remove(99);
+            
+            Assert.IsFalse(removed);
+            Assert.AreEqual(3, stack.Count);
+        }
+        
+        [Test]
+        public void RemoveAt_ShouldRemoveItemAtStackIndex() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            stack.RemoveAt(1);
+            
+            Assert.AreEqual(2, stack.Count);
+            CollectionAssert.AreEqual(new[] { 3, 1 }, stack.ToArray());
+        }
+        
+        [Test]
+        public void RemoveAt_ShouldThrowException_WhenInvalidIndex() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            Assert.Throws<ArgumentOutOfRangeException>(() => stack.RemoveAt(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => stack.RemoveAt(3));
+        }
+        
+        [Test]
+        public void TryPeek_ShouldReturnFalse_WhenEmpty() {
+            var stack = new UniqueStack<int>();
+            Assert.IsFalse(stack.TryPeek(out int _));
+        }
+        
+        [Test]
+        public void TryPeek_ShouldReturnTopItemWithoutRemoving() {
+            var stack = new UniqueStack<int>(new[] { 1, 2, 3 });
+            Assert.IsTrue(stack.TryPeek(out int top));
+            Assert.AreEqual(3, top);
+            Assert.AreEqual(3, stack.Count);
+        }
+    }
+}
---
+++ Packages/com.soratehk.sorafoundation/Runtime/Collections/UniqueStack.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace SoraTehk.Collections {
+    // TODO: Nullable item, thread-safe
+    public partial class UniqueStack<T> where T : notnull {
+        private readonly List<T> m_List;
+        private readonly HashSet<T> m_Set;
+        
+        public UniqueStack() : this(new List<T>()) { }
+        public UniqueStack(IEnumerable<T> collection) {
+            m_List = new List<T>();
+            m_Set = new HashSet<T>();
+            
+            foreach (var item in collection) {
+                Add(item);
+            }
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Push(T item) {
+            Remove(item);
+            Add(item);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T Pop() {
+            if (!TryPeek(out var item)) throw new InvalidOperationException("Stack is empty.");
+            
+            RemoveAt(0);
+            return item;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryPeek([NotNullWhen(true)] out T? item) {
+            if (m_List.Count == 0) {
+                item = default!;
+                return false;
+            }
+            
+            item = m_List[^1];
+            return true;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryPeek(int stackIndex, [NotNullWhen(true)] out T? item) {
+            if (m_List.Count == 0) {
+                item = default!;
+                return false;
+            }
+            
+            item = m_List[^(stackIndex - 1)];
+            return true;
+        }
+        
+        public int IndexOf(T item) {
+            int listIndex = m_List.LastIndexOf(item);
+            return listIndex < 0
+                ? -1
+                : m_List.Count - 1 - listIndex;
+        }
+        
+        public void Insert(int stackIndex, T item) {
+            if (stackIndex < 0 || stackIndex > Count) throw new ArgumentOutOfRangeException(nameof(stackIndex));
+            Remove(item);
+            int listIndex = m_List.Count - stackIndex;
+            
+            m_List.Insert(listIndex, item);
+            m_Set.Add(item);
+        }
+        
+        public void RemoveAt(int stackIndex) {
+            if (stackIndex < 0 || stackIndex >= Count) throw new ArgumentOutOfRangeException(nameof(stackIndex));
+            
+            int listIndex = m_List.Count - 1 - stackIndex;
+            T item = m_List[listIndex];
+            m_List.RemoveAt(listIndex);
+            m_Set.Remove(item);
+        }
+    }
+    
+    public partial class UniqueStack<T> : ICollection<T> {
+        public int Count => m_List.Count;
+        public bool IsReadOnly => false;
+        
+        public void Add(T item) {
+            if (Contains(item)) return;
+            
+            m_List.Add(item);
+            m_Set.Add(item);
+        }
+        
+        public void Clear() {
+            m_List.Clear();
+            m_Set.Clear();
+        }
+        
+        public bool Contains(T item) => m_Set.Contains(item);
+        
+        public void CopyTo(T[] array, int arrayIndex) {
+            int j = 0;
+            for (int i = m_List.Count - 1; i >= 0; i--) {
+                array[arrayIndex + j] = m_List[i];
+                j++;
+            }
+        }
+        
+        public bool Remove(T item) {
+            if (!Contains(item)) {
+                return false;
+            }
+            
+            int listIndex = m_List.LastIndexOf(item);
+            m_List.RemoveAt(listIndex);
+            m_Set.Remove(item);
+            return true;
+        }
+        
+        public IEnumerator<T> GetEnumerator() {
+            for (int i = m_List.Count - 1; i >= 0; i--) {
+                yield return m_List[i];
+            }
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    
+    public partial class UniqueStack<T> : IReadOnlyCollection<T> { } // Shared with ICollection<T>
+}
